4206ad1cbc1ced63d734f84e87d6d50db0b859cf
NaheedRayan <naheed28ray@protonmail.com>
2021-04-13 20:04:47 +0600

    added

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 5000
+
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', (req, res) => {
+
+    console.log('A get request has been made')
+    // res.send('Hello from node js from naheed')
+    res.sendFile(path.join(__dirname + '/home.html'));
+    
+    
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
