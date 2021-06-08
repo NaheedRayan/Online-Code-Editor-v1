68a3dc3d04c1c69315b651ba3eaf197436655d6d
NaheedRayan <naheed28ray@protonmail.com>
2021-06-08 21:07:17 +0600

    compression

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ const path = require('path')
 // const cors = require('cors')
 const port = process.env.PORT || 5000;
 // const https = require('https');
-
+const compression = require('compression')
 
 
 
 app.use(express.static(__dirname + '/static'));
+
+app.use(compression({
+    level: 9,
+}))
 
 app.get('/', (req, res) => {
 
     console.log('A get request has been made')
     // res.send('Hello from node js from naheed')
     res.sendFile(path.join(__dirname + '/home.html'));
-    
-    
+
+
 })
 
 app.listen(port, "0.0.0.0", () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
-
