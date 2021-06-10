be585bfbd53b95843bccb0396a19e9022c2fbdbf
NaheedRayan <naheed28ray@protonmail.com>
2021-06-11 03:55:02 +0600

    added favicon

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const favicon = require('serve-favicon')
 // const cors = require('cors')
 const port = process.env.PORT || 5000;
 // const https = require('https');
@@ -13,6 +14,8 @@ app.use(express.static(__dirname + '/static'));
 app.use(compression({
     level: 7,
 }))
+
+app.use(favicon(path.join(__dirname+'/favicon.ico')))
 
 app.get('/', (req, res) => {
 
